10e1910fc0efd8b750121cdfcd9a32b74c72eb6d
Akramjon Sattorov <aura.sttn@gmail.com>
2020-11-03 12:19:37 +0500

    solved

--- src/aurabro/Main.java
+++ src/aurabro/Main.java
@@ -5,5 +5,16 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("lsklakf");
-    }
+        Person human = new Person();
+        human.name = "ksad";
+        human.gender = 'm';
+        human.height = 1.76;
+        System.out.println("рост:"+human.height+"имя"+human.name+"пол"+human.gender);
+    }}
+    class Person {
+        String name;
+        int age;
+        char gender;
+        double height;
+
 }
